33820a11a06b7358a3455be844f5a8866e8813a9
Richard Huynh <voxlympha@gmail.com>
2012-12-17 06:58:43 +1100

    Stop building asusdec for now

--- device_tf201.mk
+++ device_tf201.mk
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
 # Build Some Extra Apps
 PRODUCT_PACKAGES += \
     Torch \
-    com.cyanogenmod.asusdec \
+#    com.cyanogenmod.asusdec \
     libasusdec_jni
 
 # Properties specific for this device
