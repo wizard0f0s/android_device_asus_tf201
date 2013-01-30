3ff7a2961fe5e7f60973c2557560b7ba433ba0d5
Jorge Ruesga <jorge@ruesga.com>
2013-01-30 23:59:43 +0100

    TF700T: Device specific dock battery support
    This change adds the specific dock battery tf700t device implementation.
    
    Patchset 2: Remove whitespaces
    Patchset 3: Transformers backwards compatibility
    Patchset 4: Fix plugged extra (aosp expect int not boolean)
    
    This change requires http://review.cyanogenmod.org/#/c/31040/
    
    Change-Id: Ieaa51a1a2472ae8bdc2c1bd7e9a40bf0b0303a21
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- asusdec/java/com/cyanogenmod/asusdec/KeyHandler.java
+++ asusdec/java/com/cyanogenmod/asusdec/KeyHandler.java
@@ -73,7 +73,7 @@ public final class KeyHandler implements DeviceKeyHandler {
     private IPowerManager mPowerManager;
 
     static {
-        System.loadLibrary("asusdec_jni");
+        AsusdecNative.loadAsusdecLib();
     }
 
     public KeyHandler(Context context) {
