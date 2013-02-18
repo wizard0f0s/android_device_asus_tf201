2108cd3b03edc0e223f85ffe06376f0c59ec7758
Richard Huynh <voxlympha@gmail.com>
2013-02-18 15:27:44 +1100

    Change BTM_DEF_LOCAL_NAME
    To be in line with what is listed on get.cm

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "Asus Eeepad Transformer Prime TF201"
+#define BTM_DEF_LOCAL_NAME "ASUS EeePad Transformer Prime"
 
 // Networking, Capturing, Object Transfer
 // MAJOR CLASS: COMPUTER
